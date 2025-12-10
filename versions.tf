92c43da9de1909fb82284f6b448a03b0862234b7
Cristian Beraha <cristian@cloudops.works>
2025-12-10 10:03:32 -0300

    chore: Upgrade repository from template, version: v1.3.7

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.83"
+      version = "~> 6.20"
     }
     random = {
       source  = "hashicorp/random"
